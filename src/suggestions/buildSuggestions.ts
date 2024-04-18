d846ee03672d780cdbe70ccea9084e6bd8d64a41
papipsycho <julien.romagnoli@akktis.com>
2024-04-18 17:14:56 +0700

    Update buildSuggestions.ts

--- src/suggestions/buildSuggestions.ts
+++ src/suggestions/buildSuggestions.ts
@@ -20,7 +20,7 @@ import { Suggestion } from './types.js'
  */
 export function buildSuggestions(
   registry: ParameterTypeRegistry,
-  stepTexts: readonly any[],
+  stepTexts: readonly Object[],
   expressions: readonly Expression[],
   maxChoices = 10
 ): readonly Suggestion[] {
